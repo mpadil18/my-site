9081eba419883a39e78511b94b2ced5ace8c0453
mpadil18 <mpadil18@ucsc.edu>
2024-07-18 17:38:34 -0700

    Updated routing, post display, error handling

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -2,26 +2,87 @@
 "use client"; 
 
 import Image from 'next/image'
-import { useSearchParams } from 'next/navigation'
+import { usePathname } from 'next/navigation'
+import Records from "../../../data/blog_posts.json"
+import { useState, useEffect } from "react";
 
 import styles from "./page.module.css"
 
+interface PostProps {
+    title: string;
+    date: string;
+    content:Array<
+    {
+        subHeader?: string;
+        subText: string;
+    }
+    >;
+}
+
 export default function BlogDetailView() {
-    const searchParams = useSearchParams()
- 
-    const search = searchParams.get('search')
-    let property1 = searchParams.get("id");
+    const pathname = usePathname().replaceAll("/blog/", "").replaceAll("_", " ")
+    const [currContent, setCurrContent] = useState<PostProps>({title:"", date:"", content:[{"subText":""}]} || {});
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        checkIfPostExists();
+    })
+
+    // Function checks JSON file for the blog post title in the URL.
+    // TODO: replace this with a PSQL query
+    function checkIfPostExists():void {
+        if (pathname) {
+            const valsFromRecords = Object.values(Records)
+            for (const data of valsFromRecords) {
+                for (const item of data)
+                    if (item.title == (pathname)) {
+                        setCurrContent(item);
+                        console.log(item)
+                        setLoading(false)
+                        return;
+                }
+    
+            }
+        }
+        setLoading(false)
+    }
+
     return (
         <main>
-            <div className={styles.BlogDetailView}>
-                <div className={styles.columnContainer}>
-                    <div className={styles.postHeader}></div>
-                    <p style={{color:"white"}}>Hello world {property1}</p>
-                    <div className={styles.postDate}></div>
+            {/* If page is loading: display loading sign.
+                When page is loaded:
+                    If blog post found: render it
+                    Else: Display error message
+            */}
+            {loading ?
+            <p style={{color:"white"}}>loading</p> 
+            :
+            (currContent.title).length > 0 ?
+                <div className={styles.BlogDetailView}>
+                    <div className={styles.columnContainer}>
+                        <div className={styles.headerAbout}>{currContent.title}</div>
+                        <div className={styles.postDate}>{currContent.date}</div>
+                    </div>
+                    <hr></hr>
+
+                    {
+                        (currContent.content).map((item, index) =>
+                            <div className={styles.postSection} key={index}>
+                                {
+                                    // Render subHeader if exists. Else, nothing
+                                    item.subHeader ? <div className={styles.subHeader}>{item.subHeader}</div>
+                                    :
+                                    null
+                                }
+                                <div className={styles.subText}>{item.subText}</div>
+                            </div>
+                        )
+                    }
+                    
                 </div>
-                <hr></hr>
-                
-            </div>
+                :
+                <p style={{color:"white"}}>Sorry, {pathname} not found</p>
+            }
         </main>
     )
 }
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,13 +2,15 @@
 "use client"; 
 
 import Image from 'next/image'
+import Records from "../../data/blog_posts.json"
 
 import styles from "./page.module.css"
 import Link from 'next/link'
 
-const projectInfo = {"2023": [["Review - Moonridge Cafe", "Proin nec volutpat arcu. Proin a sollicitudin lectus, et sodales neque. Vestibulum eu finibus ex."], ["Review - Everything Everywhere All At Once", "Proin nec volutpat arcu. Proin a sollicitudin lectus, et sodales neque. Vestibulum eu finibus ex."], ["Ode to Cheese", "Proin nec volutpat arcu. Proin a sollicitudin lectus, et sodales neque. Vestibulum eu finibus ex."], ["Some Large Title", "Proin nec volutpat arcu. Proin a sollicitudin lectus, et sodales neque. Vestibulum eu finibus ex."]]}
-
+// const projectInfo = {"2023": [["Review - Moonridge Cafe", "Proin nec volutpat arcu. Proin a sollicitudin lectus, et sodales neque. Vestibulum eu finibus ex."], ["Review - Everything Everywhere All At Once", "Proin nec volutpat arcu. Proin a sollicitudin lectus, et sodales neque. Vestibulum eu finibus ex."], ["Ode to Cheese", "Proin nec volutpat arcu. Proin a sollicitudin lectus, et sodales neque. Vestibulum eu finibus ex."], ["Some Large Title", "Proin nec volutpat arcu. Proin a sollicitudin lectus, et sodales neque. Vestibulum eu finibus ex."]]}
+const projectInfo = Records
 export default function Blog() {
+  console.log(Records);
   return (
     <main>
       <div className={styles.Blog}>
@@ -23,25 +25,22 @@ export default function Blog() {
               // <div key={index}>
               <Link
                 href={
-                {pathname: `/blog/${project[0].replaceAll(" ", "_").replaceAll("-", "_")}`,
-                 query: {
-                  id: index
-                 }
-                }} key={project[0].replaceAll(" ", "_").replaceAll("-", "_")} style={{ textDecoration: 'none', color: 'white' }}>
+                {pathname: `/blog/${project.title.replaceAll(" ", "_")}`
+                }} key={project.title.replaceAll(" ", "_")} style={{ textDecoration: 'none', color: 'white' }}>
                 <div className={styles.project}>
                   <div className={`${styles.projectImg} ${styles.imageContainer}`}>
                     <span className={styles.image}>
                       <Image 
-                          src={`/${project[0]}.png`}
-                          alt={`Picture of project - /${project[0]}`}
+                          src={`/${project.title}.png`}
+                          alt={`Picture of project - /${project.title}`}
                           width={270}
                           height={270}
                       />       
                       </span>
                       <div className={styles.overlay}><p className={styles.date}>Mar 10</p></div>  
                   </div>
-                  <div className={styles.projectTitle}>{project[0].slice(0, 33)}{(project[0].length > (project[1].slice(0, 33)).length) ? "..." : ""}</div>
-                  <div className={styles.projectDesc}>{project[1]}</div>        
+                  <div className={styles.projectTitle}>{project.title.slice(0, 33)}{(project.title.length > (project.title.slice(0, 33)).length) ? "..." : ""}</div>
+                  <div className={styles.projectDesc}>{project.content[0].subText.slice(0,150)}</div>        
                 </div>
                 </Link>
                 // </div>
@@ -50,23 +49,7 @@ export default function Blog() {
           </div>
         )
       }
-        {/* {projectInfo.map((project, index) => (
-          <div className="project" key={index}>
-          <div className="projectYear">{project[0]}</div>
-          <div className="projectImg">
-              <Image 
-                  src={`/${project[1]}.png`}
-                  alt="Picture of Project"
-                  width={270}
-                  height={270}
-              />
-          </div>
-          <div className="projectTitle">{project[1].slice(0, 33)}{(project[1].length > (project[1].slice(0, 33)).length) ? "..." : ""}</div>
-          <div className="projectDesc">{project[2]}</div>
-          </div>
-        ))} */}
       </div>
-      {/* </div> */}
     </main>
   );
 }
