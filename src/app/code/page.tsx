a491f18c4b46e6b68e8491e9c8f089e6115570cd
Maritza Padilla <mpadil18@ucsc.edu>
2024-10-12 14:06:00 -0700

    Added Project Links to Code

--- src/app/code/page.tsx
+++ src/app/code/page.tsx
@@ -1,5 +1,5 @@
-
 import Image from 'next/image'
+import Link from 'next/link'
 
 import styles from "./page.module.css"
 
@@ -16,16 +16,24 @@ export default function Code() {
         {projectInfo.map((project, index) => (
           <div className={`${(index % 4 == 0 ? styles.projectNoLeftMargin : (index - 3) % 4 == 0 ? styles.projectNoRightMargin : styles.project)} ${(index % 2 == 0 ? styles.rightMarginMobile : styles.leftMarginMobile)}`} key={index}>
           <div className={`${styles.projectImg} ${styles.imageContainer}`}>
-              <Image 
-                  src={`/my-site/project_images/${project.project_id}.png`}
-                  alt="Picture of Project"
-                  width={270}
-                  height={270}
-                  style={{ width: '100%', height: '100%' }}
-              />
+              <Link href={project.project_url} passHref legacyBehavior>
+              <a target="_blank" style={{"color": "white"}}>
+                <Image 
+                    src={`/my-site/project_images/${project.project_id}.png`}
+                    alt="Picture of Project"
+                    width={270}
+                    height={270}
+                    style={{ width: '100%', height: '100%' }}
+                />
               <div className={styles.overlay}><p className={styles.date}>{project.project_year}</p></div>  
+              </a>
+              </Link>
           </div>
-          <div className={styles.projectTitle}>{project.project_title}</div>
+          <Link href={project.project_url} passHref legacyBehavior>
+            <a target="_blank" style={{"textDecoration": "none"}}>
+              <div className={styles.projectTitle}>{project.project_title}</div>
+            </a>
+          </Link>
           <div className={styles.projectDesc}>{project.project_desc}</div>
           </div>
         ))}
